c5fa3323d644a0ae5f7b069794715ec74458d717
arthyom <a.gonzalezgavina@ugto.mx>
2020-09-11 14:25:17 -0500

    agregando los modelos

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -10,5 +10,11 @@ class IndexController extends AppController
     public function index()
     {
 
+        $usrs = ( new usuarios)->find();
+        $rls  = ( new roles)->find();
+        View::select(null);
+
+        echo var_dump( $rls );
+
     }
 }
